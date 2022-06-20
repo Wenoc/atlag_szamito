e5484697ef17b2cff29971b95f16f6ba9d9ce9dc
Wenoc <norbert.kis@tsis.edu.rs>
2022-06-20 14:29:17 +0200

    lehet osztalyzatokat bevinni es kitorolni mindenhonnan

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let pit = document.getElementById('pit');
 atlagszamitas();
 
 function addItem() {
-    var a = document.getElementById("pit");
+    var a = document.getElementById(document.getElementById('valasztas').value);
     var candidate = document.getElementById("candidate");
     var span = document.createElement("span");
     span.setAttribute('onclick', 'setAttribute(\'hidden\', \'\')');
@@ -13,17 +13,27 @@ function addItem() {
 
 function atlagszamitas(){
     let osztalyzatok = [];
-    for (let i = 0; i < pit.children.length; i++) {
-        if(!pit.children[i].hasAttribute('hidden')){
-            console.log(pit.children[i].textContent);
-            osztalyzatok.push(parseInt(pit.children[i].textContent, 10));
+    let tantargyak = ['pit', 'web_programozas', 'matematika', 'szociologia', 'valasztott_matematika', 'szamitogepes_rendszerek', 'adatbazisok', 'programozas', 'torna', 'magyar_nyelv', 'szerb_nyelv', 'angol_nyelv', 'magaviselet']
+
+    for(let j = 0; j < tantargyak.length; j++){
+        let tantargy = document.getElementById(tantargyak[j]);
+        for (let i = 0; i < tantargy.children.length; i++) {
+            if(!tantargy.children[i].hasAttribute('hidden')){
+                osztalyzatok.push(parseInt(tantargy.children[i].textContent, 10));
+            }
         }
+        let osszes = 0;
+        for(let i = 0; i < osztalyzatok.length; i++){
+            osszes += osztalyzatok[i]
+        }
+        if(osszes > 0){
+            document.getElementById(tantargyak[j] + '_atlag').textContent = (osszes/osztalyzatok.length).toFixed(2);
+        }
+        else{
+            document.getElementById(tantargyak[j] + '_atlag').textContent = '/';
+        }
+        osztalyzatok = [];
     }
-    let osszes = 0;
-    for(let i = 0; i < osztalyzatok.length; i++){
-        osszes += osztalyzatok[i]
-    }
-    document.getElementById('pit_atlag').textContent = (osszes/osztalyzatok.length).toFixed(2);
 }
 
 pit.addEventListener('click', () => {
